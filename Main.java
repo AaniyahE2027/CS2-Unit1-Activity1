1602e9b0d6875592288d6b73800bb31b3c6eb671
Aaniyah <aaniyah.ellis2027@gbwl.org>
2025-09-18 18:24:16 +0000

    added text

--- Main.java
+++ Main.java
@@ -11,9 +11,31 @@ public class Main {
       //System.in refers to the physical keyboard!
 
       //3. Use the scanner to take input
-      System.out.println("Type your name then press enter: ");eh
+      System.out.println("Type your name then press enter: ");
       String player = input.nextLine();
       System.out.println("Welcome, " + player);
+
+      // Create variables for each placeholder word
+      String adj1 = "warm";
+      String adj2 = "long";
+      String food = "pizza";
+      String adj3 = "yummy";
+      String emotion = "happy";
+      String name = "Emma";
+      String emotionEndingIng = "upseting";
+      String personailtyTrait = "a brat";
+      String location = "home";
+      String activity = "slept";
+
+      //Print your story line by line
+      System.out.println("It was a " + adj1 + " September.");
+      System.out.println("We had to go to school and why knew it was going to be a" + adj2 + " day.");
+      System.out.println("It was lunch time and I had a " + food + " which was " + adj3 + ".");
+      System.out.println("This made me feel " + emotion + ".");
+      System.out.println("While I was running up the stairs. I ran into " + name + ".");
+      System.out.println("This was " + emotionEndingIng + " because " + name + " was" + personailtyTrait + ".");
+      System.out.println("Finally, the day was over and I went " + location + " and " + activity);
+
 
    }
 }
